af387d69ff7f53f7cc7f5311eff54b88d1690f9b
sushil <sushils@interrait.com>
2018-02-09 12:19:31 +0530

    Setup start script and hiroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
@@ -59,6 +61,6 @@ app.get('/bad', (req, res) => {
     res.send("Unable to fulfill this request!");
 })
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000!")
+app.listen(port, () => {
+    console.log(`Listening on port ${port}!`)
 });
